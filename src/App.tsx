3ebd0e2c42130449e8d734ffae1a27d60569e7b6
Paul Holt <paul-ho@lightflows.co.uk>
2024-01-27 02:43:11 +0000

    Add clickable area to carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import './App.css'
 import CarouselWrapper from "./components/Carousel/CarouselWrapper";
 
 const App = () => {
-  return <CarouselWrapper />
+  return (
+      <div className='grid-container'>
+          <div className='container'>
+              <h1>Carousel Test Site</h1>
+          </div>
+          <CarouselWrapper />
+      </div>
+  )
 }
 
 export default App
--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -7,15 +7,33 @@ interface CarouselProps {
 }
 
 const Carousel: FC<CarouselProps> = ({items}) => {
-    const {position} = usePosition();
+    const {position, setPosition} = usePosition();
+
+    const handlePrevClick = () => {
+        if(position === 1) {
+            return;
+        }
+        setPosition(position - 1);
+    };
+
+    const handleNextClick = () => {
+        if(position === items.length) {
+            return;
+        }
+
+        setPosition(position + 1);
+    };
 
     return (
         <div className='carousel'>
+            {position > 1 ? <div className="clickable-area clickable-area_left" onClick={handlePrevClick}></div> : null}
             <main id="carousel" style={{ '--position': position }}>
                 {items.map((item, index) => {
                     return <Card key={index} title={item.title} body={item.body} cardPosition={index + 1}/>
                 })}
             </main>
+            {position < items.length ? <div className="clickable-area clickable-area_right" onClick={handleNextClick}></div> : null}
+
             <RadioWrapper count={items.length} />
         </div>
     );
