23aa65ee1e508b80e4e0860f41d15ecb5f8ab626
Paul Holt <paul-ho@lightflows.co.uk>
2024-02-09 23:21:31 +0000

    Move items to own file

---
+++ src/data/items.ts
@@ -0,0 +1,37 @@
+export const items = [
+    {
+        title: "Stripes",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus rhoncus aliquam. Vestibulum " +
+            "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse eget lorem magna. ",
+        src: "http://placekitten.com/g/400/400"
+    },
+    {
+        title: "Soggs",
+        body: "In hac habitasse platea dictumst. Phasellus odio est, varius vitae leo in, rutrum convallis eros. " +
+            "Integer scelerisque arcu id molestie maximus.",
+        src: "http://placekitten.com/g/500/500"
+    },
+    {
+        title: "Coco",
+        body: "Duis laoreet vulputate molestie. Donec auctor libero purus, a efficitur diam convallis fermentum.",
+        src: "http://placekitten.com/g/600/600"
+    },
+    {
+        title: "Buttons",
+        body: "Donec pharetra, leo in sodales varius, enim mauris commodo sapien, eu rutrum ligula tortor a urna.",
+        src: "http://placekitten.com/g/700/700"
+    },
+    {
+        title: "Kate",
+        body: "Vestibulum rutrum elit arcu, non dignissim nisi scelerisque pretium. Aliquam accumsan tortor est, quis " +
+            "malesuada diam varius sit amet.",
+        src: "http://placekitten.com/g/800/800"
+    },
+    {
+        title: "Sooty",
+        body: "Nullam dapibus dolor sit amet ex semper aliquet. Nunc euismod pellentesque arcu, ac volutpat justo " +
+            "consequat at. Aenean cursus libero non accumsan ultrices. ",
+        src: "http://placekitten.com/g/900/900"
+    },
+
+];
