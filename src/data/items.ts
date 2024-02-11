1f7631ede068e6ac0a23a81ae99e21da4ddaedc0
Paul Holt <paul-ho@lightflows.co.uk>
2024-02-11 23:38:00 +0000

    Remove cute cat names

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,28 +1,28 @@
 export const items = [
     {
-        title: "Stripes",
+        title: "Lorem ipsum dolor",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas maximus rhoncus aliquam. Vestibulum " +
             "ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse eget lorem magna. ",
         src: "http://placekitten.com/g/400/400"
     },
     {
-        title: "Soggs",
+        title: "In hac habitasse",
         body: "In hac habitasse platea dictumst. Phasellus odio est, varius vitae leo in, rutrum convallis eros. " +
             "Integer scelerisque arcu id molestie maximus.",
         src: "http://placekitten.com/g/500/500"
     },
     {
-        title: "Coco",
+        title: "Duis laoreet vulputate",
         body: "Duis laoreet vulputate molestie. Donec auctor libero purus, a efficitur diam convallis fermentum.",
         src: "http://placekitten.com/g/600/600"
     },
     {
-        title: "Buttons",
+        title: "Donec pharetra",
         body: "Donec pharetra, leo in sodales varius, enim mauris commodo sapien, eu rutrum ligula tortor a urna.",
         src: "http://placekitten.com/g/700/700"
     },
     {
-        title: "Kate",
+        title: "Vestibulum rutrum",
         body: "Vestibulum rutrum elit arcu, non dignissim nisi scelerisque pretium. Aliquam accumsan tortor est, quis " +
             "malesuada diam varius sit amet.",
         src: "http://placekitten.com/g/800/800"
